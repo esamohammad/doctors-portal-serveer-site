e1a22161fe22cf14be5b43f70da3c1ed54655d7f
mdesabd <esa.ce2020@gmail.com>
2022-12-20 19:50:16 +0600

    46 done database connected

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,15 +12,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS} @cluster0.r9bykuu.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9bykuu.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database connected Alhamdulillah Alhamdulillah Alhamdullillah")
+  }
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
-res.send('Hello From Doctor Uncle!')
+  res.send('Hello From Doctor Uncle!')
 })
 
 app.listen(port, () => {
-console.log(`Doctors App listening on port ${port}`)
+  console.log(`Doctors App listening on port ${port}`)
 })
+
+
 
