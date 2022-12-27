12b455b2c396c3d24dc30f96f9fb808f608c1160
mdesabd <esa.ce2020@gmail.com>
2022-12-28 05:38:18 +0600

    admin verifi kora

--- index.js
+++ index.js
@@ -56,6 +56,31 @@ async function run() {
     const doctorCollection = client.db('doctors_portal').collection('doctors');
 
 
+    //admin varifi for add doctor
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     app.get('/service', async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query).project({ name: 1 });
@@ -88,47 +113,15 @@ async function run() {
 
     //admin role----
     //user ke server a update ar por admin field toiri
-    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      // const user = req.body;// body theke ar kisu nibo na 
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({ email: requester });
-
-      if (requesterAccount.role === 'admin') {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: 'admin' },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-
-      }
-      else {
-        res.status(403).send({ message: 'forbidden' });
-      }
-
-
-
-      /*
-      
-            const filter = { email: email };
-            // const options = { upsert: true }; // atao no send
-            const updateDoc = {
-              $set: {
-                role: 'admin' //role holo se akjon admin roomer owner
-              },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            //option tao bad
-            // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            //akhne tocke issu korar kono karbar nai .
-            res.send(result); //result take pathaye dibo.
-      */
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
-
-
-
-
 
 
     //uer ke server a update
@@ -225,7 +218,7 @@ async function run() {
     })
 
     //doctor image ,nam ,file
-    app.post('/doctor', async (req, res) => {
+    app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
       const doctor = req.body;
       const result = await doctorCollection.insertOne(doctor);
       res.send(result);
